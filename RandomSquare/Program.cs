36e5361ea05dfd362bbe06de6065d59eb7eca946
Grace Sutherland <rgs1690@gmail.com>
2022-01-24 20:32:12 -0600

    Add project files.

---
+++ RandomSquare/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random random = new Random();
+var list = new List<int>();
+int min = 1;
+int max = 50;
+for (int i = 0; i < 21; i++)
+{
+    list.Add(random.Next(min, max));
+   
+    
+}
+//foreach (int i in list)
+//{
+//    Console.WriteLine(i);
+//}
+
+IEnumerable<int> numbersSquared = list.Select(number => number * number);
+
+//foreach (int number in numbersSquared)
+//{
+//    Console.WriteLine(number);
+//}
+
+var onlyEvens = numbersSquared.Where(n => n % 2 == 0);
+foreach (int number in onlyEvens)
+{
+    Console.WriteLine(number);
+}
